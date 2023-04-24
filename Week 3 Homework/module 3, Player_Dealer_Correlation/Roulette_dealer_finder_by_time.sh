5d7188e70848d2b84f7624b467053086eb4905e9
Zackary-Tullar <107084943+Zackary-Tullar@users.noreply.github.com>
2023-04-24 13:23:36 -0400

    del

--- Week 3 Homework/module 3, Player_Dealer_Correlation/Roulette_dealer_finder_by_time.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# analyze the employee schedule to find the roulette dealer at a specific time.
-
-cat $1_Dealer_schedule | awk -F" " '{print $1, $2, $5, $6}'| grep "$2"
-
--- Week 7 Homework/Module 7, Challenge/enum.acls.ps1
+++
@@ -1,7 +0,0 @@
-﻿# Assign Directory to current directory
-$directory = $PSScriptRoot
-
-$var = Get-ChildItem -Recurse -Path $directory
-foreach ($item in $var) {
-Get-Acl $itm.Fullname
-}
